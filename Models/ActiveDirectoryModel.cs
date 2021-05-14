de1e3f9b7bf960514c40e7202ad583cb00b21ec3
Shockley <shocklam@ucmail.uc.edu>
2021-05-14 14:04:35 -0400

    a test

--- Models/ActiveDirectoryModel.cs
+++ Models/ActiveDirectoryModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.DirectoryServices;
 using System.Web;
-
+// test
 namespace COMAPI.Models
 {
     public class ActiveDirectoryModel : BaseModel
